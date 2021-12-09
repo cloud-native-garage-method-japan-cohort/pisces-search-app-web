f103a8243e6152c23b717c2cef3f52756208fd45
Keito Osaki <Keito.Osaki@ibm.com>
2021-12-09 18:08:09 +0900

    update: improve header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,33 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
 import Style from "./Header.module.css";
 
+const useStyles = makeStyles({
+  header: {
+    backgroundColor: "#333333",
+  },
+  title: {
+    fontWeight: "bold",
+  },
+});
+
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <div className={Style.header}>
-      <div className={Style.item}>
-        <Link to="/"> 営業資料検索くん </Link>
-      </div>
-    </div>
+    <AppBar className={classes.header} position="static">
+      <Toolbar>
+        <Typography className={classes.title} variant="h6">
+          営業資料検索くん
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
